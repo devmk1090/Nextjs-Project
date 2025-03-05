8de8229450ccd2db09a0d031e6912d35d7eccddc
devmk1090 <devmk1090@gmail.com>
2025-03-06 01:47:01 +0900

    - 애드센스 코드 스니펫 추가 - Ads.txt 스니펫 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import DrawerNavigation from "./components/DrawerNavigation";
+import GoogleAdsense from "./components/GoogleAdsense";
 
 const inter = Inter({ subsets: ["latin"] });
 
 
 export const metadata: Metadata = {
   title: "만 나이 계산기",
-  description: "만 나이를 계산해주는 웹 애플리케이션입니다.",
+  description: "만 나이, 띠, 별자리 등 다양한 계산기를 제공합니다",
 };
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>
+        <GoogleAdsense />
         <DrawerNavigation />
         {children}
       </body>
